2bff30a95a620af38ab5c6d34a821b3720fd0f9e
ks-vijai <vijaiks.19cse@kongu.edu>
2023-02-01 22:39:47 +0530

    feat: home-page: add overall progress details
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { FcCheckmark } from "react-icons/fc";
+import { IoPeopleSharp } from "react-icons/io5";
 
 function Homepage() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="overall-board">
+        <div className="date-day">Wednesday, February 1</div>
+        <div className="greeting-message">Good Evening, Vijai</div>
+        <div className="overall-dashboard">
+          <div>My Tasks</div>
+          <div className="task-progress-list">
+            <FcCheckmark className="progress-details" />
+            <span className="progress-details"> 0</span> tasks completed
+          </div>
+          <div className="task-progress-list">
+            <IoPeopleSharp className="progress-details" />
+            <span className="progress-details"> 0</span> collaborators
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
