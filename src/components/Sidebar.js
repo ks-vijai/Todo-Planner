c5edd471adea155c2940c9f385d602d9a7a397eb
ks-vijai <vijaiks.19cse@kongu.edu>
2023-02-15 14:54:26 +0530

    refactor: nav-bar: change sidebar to title bar
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,10 @@
 import React, { useState, useEffect } from "react";
-import {
-  FaTh,
-  FaRegChartBar,
-  FaThList,
-  FaUserAlt,
-  FaShoppingBag,
-  FaBars,
-} from "react-icons/fa";
-import { VscAccount } from "react-icons/vsc";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ProfileCard from "./ProfileCard";
+import { AiOutlineLogout } from "react-icons/ai";
 
 function Sidebar({ children }) {
-  const [isOpen, setIsOpen] = useState(true);
   const [showCard, setShowCard] = useState(false);
-  const navigate = useNavigate();
-  const toggle = () => setIsOpen(!isOpen);
-  const routeToHome = () => {
-    navigate("/");
-  };
 
   useEffect(() => {
     const concernedElement = document.querySelector(".user-account");
@@ -35,66 +21,55 @@ function Sidebar({ children }) {
     {
       path: "/",
       name: "Home",
-      icon: <FaTh />,
     },
     {
       path: "/dashboard",
       name: "Dashboard",
-      icon: <FaThList />,
     },
     {
       path: "/tasks",
       name: "Tasks",
-      icon: <FaUserAlt />,
     },
     {
       path: "/analytics",
       name: "Analytics",
-      icon: <FaRegChartBar />,
     },
     {
-      path: "/goals",
-      name: "Goals",
-      icon: <FaShoppingBag />,
+      path: "/calendar",
+      name: "Calendar",
     },
   ];
 
   return (
-    <div className="sidebar-container">
-      <div className={isOpen ? "sidebar" : "sidebar sidebar-child"}>
-        <div className="navigation-section">
-          <h1
-            className={isOpen ? "planner-logo" : "hide-elements"}
-            onClick={routeToHome}
-          >
-            Logo
-          </h1>
-          <div className={isOpen ? "hamburger" : "hamburger move-bars"}>
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
-        {menuItems.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className="nav-link"
-            activeclassname="active"
-          >
-            <div className="nav-icon">{item.icon}</div>
-            <div className={isOpen ? "nav-link-text" : "hide-elements"}>
-              {item.name}
-            </div>
-          </NavLink>
-        ))}
-      </div>
-      <main className="child-page">
+    <div>
+      <header>
         <div className="title-bar">
-          <div className="planner-title">Title Bar</div>
-          <div className="profile-icon">
-            <VscAccount className="user-account" />
-            {showCard && <ProfileCard />}
+          <div className="nav-link-items">
+            {menuItems.map((item, index) => (
+              <NavLink
+                to={item.path}
+                key={index}
+                className="nav-link"
+                activeclassname="active"
+              >
+                <div className="nav-link-text">{item.name}</div>
+              </NavLink>
+            ))}
+          </div>
+          <div>
+            <div className="profile-icon">
+              <div className="user-account">
+                <span className="user-name">VS</span>
+                {showCard && <ProfileCard />}
+              </div>
+              <div>
+                <AiOutlineLogout className="logout-button" />
+              </div>
+            </div>
           </div>
         </div>
+      </header>
+      <main>
         <div>{children}</div>
       </main>
     </div>
